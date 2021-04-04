e234c4bb341e2f828b8f81a8249544e5cc10984e
Pablo Lozano <lozano.p15@gmail.com>
2021-04-04 15:28:59 +0200

    Separate in files

--- plain_api.py
+++ plain_api.py
@@ -3,32 +3,11 @@ import os
 import json
 import yaml
 
-with open('../.twitter_keys.yaml') as file:
-    document = yaml.full_load(file)
-    bearer_token = document['search_tweets_v2']['bearer_token']
-
-search_url = "https://api.twitter.com/2/tweets/search/all"
-
-query_params = {
-        # 'query':        '(from:twitterdev -is:retweet) OR #twitterdev',
-        'query':        '(from:twitterdev)',
-        'tweet.fields': 'author_id'
-        }
-
-
-def create_url():
-    query = "from:twitterdev -is:retweet"
-    # Tweet fields are adjustable.
-    # Options include:
-    # attachments, start_time,end_time,since_id,until_id,max_results,next_token, expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fieldsauthor_id, context_annotations, conversation_id, created_at, entities, geo, id, in_reply_to_user_id, lang, non_public_metrics, organic_metrics, possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets, source, text, and withheld
-    tweet_fields = "tweet.fields=author_id"
-
-    # all_url = "https://api.twitter.com/2/tweets/search/all"
-    recent_url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}".format(
-        query, tweet_fields
-    )
-
-    return recent_url
+def load_keys():
+    with open('../.twitter_keys.yaml') as file:
+        document = yaml.full_load(file)
+        bearer_token = document['search_tweets_v2']['bearer_token']
+    return bearer_token
 
 
 def create_headers(bearer_token):
@@ -37,16 +16,73 @@ def create_headers(bearer_token):
 
 
 def connect_to_endpoint(url, headers, params):
-    response = requests.request("GET", search_url, headers=headers, params=params)
+    response = requests.request("GET", url, headers=headers, params=params)
     print(response.status_code)
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
     return response.json()
 
 
+# URLs
+#
+# Followers look-up
+def url_params_followers(user_id=865577763679252480):
+    url = "https://api.twitter.com/2/users/{}/followers".format(user_id)
+    params = {
+            'user.fields': 'created_at'
+            }
+    return url, params
+
+# Full archive search
+def url_params_full_archive():
+    url = "https://api.twitter.com/2/tweets/search/all"
+    params = {
+            'query': '(from:twitterdev -is:retweet) OR #twitterdev', 
+            'tweet.fields': 'author_id'
+            }
+    return url, params
+
+# Recent search
+def url_params_recent():
+    url = "https://api.twitter.com/2/tweets/search/recent"
+    params = {
+            'query': '(from:twitterdev -is:retweet) OR #twitterdev', 
+            'tweet.fields': 'author_id'
+            }
+    return url, params
+
+# Tweet look-up
+def url_params_regular():
+    url = "https://api.twitter.com/2/tweets"
+    params = {
+            'ids': '1278747501642657792,1255542774432063488', 
+            'tweet.fields': 'lang,author_id'
+            }
+    return url, params
+
+# User look-up
+def url_params_user():
+    url = "https://api.twitter.com/2/users/by"
+    params = {
+            'usernames': 'TwitterDev,TwitterAPI', 
+            'user.fields': 'description,created_at'
+            }
+    return url, params
+
+
 def main():
+    bearer_token = load_keys()
     headers = create_headers(bearer_token)
+
+    ### Uncomment one
+    # search_url, query_params = url_params_followers()
+    # search_url, query_params = url_params_full_archive()
+    # search_url, query_params = url_params_recent()
+    search_url, query_params = url_params_regular()
+    # search_url, query_params = url_params_user()
+
     json_response = connect_to_endpoint(search_url, headers, query_params)
+
     print(json.dumps(json_response, indent=4, sort_keys=True))
 
 
